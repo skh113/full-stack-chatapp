93db627b956d0256a6be6369db6b2aeb24d5b8b4
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-23 18:24:19 +0330

    Added Swagger UI

--- djangoChat/djangoChat/settings.py
+++ djangoChat/djangoChat/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # 3rd party apps
     "rest_framework",
+    "drf_spectacular",
     # My apps
     "server",
     "account",
@@ -121,3 +122,14 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
