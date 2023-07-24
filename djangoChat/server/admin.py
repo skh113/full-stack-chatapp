4d26e0e5a4279f646bc088f75bcffb67917a3700
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-24 18:35:27 +0330

    Added user auth for filtering

--- djangoChat/server/admin.py
+++ djangoChat/server/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import Channel, Server, Category
 
 admin.site.register(Channel)
-admin.site.register(Server)
 admin.site.register(Category)
+
+
+@admin.register(Server)
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner',
+                    'category', 'description']
+    list_editable = ['name']
+    search_fields = ['name']
+
+    list_display_links = None
+
+    def __str__(self):
+        return self.title
--- djangoChat/server/views.py
+++ djangoChat/server/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from rest_framework.response import Response
 
 from .models import Server
@@ -12,14 +14,26 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
+        by_server_id = request.query_params.get("by_server_id")
 
+        # the order of these statements matter!
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
-        if by_user:
+        if by_user and not request.user.is_authenticated:
+            raise AuthenticationFailed()
+        else:
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
-            
+
+        if by_server_id:
+            try:
+                self.queryset = self.queryset.filter(id=by_server_id)
+                if not self.queryset.exists():
+                    raise Http404
+            except ValueError:
+                raise ValidationError(detail="Value Error! the type should be int.")
+
         if qty:
             self.queryset = self.queryset[: int(qty)]
 
