9fb9325d132db7c5f6aed1714d3eccc36472e0d9
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-25 23:32:15 +0330

    Deleting icons after Category delete

--- djangoChat/server/models.py
+++ djangoChat/server/models.py
@@ -2,6 +2,7 @@ from typing import Iterable, Optional
 
 from django.conf import settings
 from django.db import models
+from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
 
 NAME_MAX_LENGTH = 100
@@ -28,6 +29,14 @@ class Category(models.Model):
                 existing.icon.delete(save=False)
 
         super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def on_delete_category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
