d3f4208d5e3bdb6f057f29e3cdc2a7c99e86e4ef
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-25 23:24:34 +0330

    Added icon to Category

--- djangoChat/server/models.py
+++ djangoChat/server/models.py
@@ -1,12 +1,33 @@
+from typing import Iterable, Optional
+
 from django.conf import settings
 from django.db import models
+from django.shortcuts import get_object_or_404
 
 NAME_MAX_LENGTH = 100
+
+
+def get_category_icon_upload_path(instance, filename) -> str:
+    return f"category/{instance.id}/category_icon/{filename}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=NAME_MAX_LENGTH)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(
+        upload_to=get_category_icon_upload_path,
+        null=True,
+        blank=True,
+    )
+
+    # delete previous icon if a new one is uploaded
+    def save(self, *args, **kwargs) -> None:
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
