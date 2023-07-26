e744ecda6c096e6554fcdc759b8193ea83ab5163
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-26 16:26:57 +0330

    Added validation

--- djangoChat/server/models.py
+++ djangoChat/server/models.py
@@ -5,7 +5,17 @@ from django.db import models
 from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
 
+from .validators import validate_icon_image_size
+
 NAME_MAX_LENGTH = 100
+
+
+def get_channel_banner_upload_path(instance, filename) -> str:
+    return f"channel/{instance.id}/channel_banner/{filename}"
+
+
+def get_channel_icon_upload_path(instance, filename) -> str:
+    return f"channel/{instance.id}/channel_icon/{filename}"
 
 
 def get_category_icon_upload_path(instance, filename) -> str:
@@ -58,10 +68,28 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="channel_owner")
     topic = models.CharField(max_length=NAME_MAX_LENGTH)
     server = models.ForeignKey(Server, on_delete=models.PROTECT, related_name="channel_server")
+    banner = models.ImageField(upload_to=get_channel_banner_upload_path, null=True, blank=True)
+    icon = models.ImageField(upload_to=get_channel_icon_upload_path, null=True, blank=True,
+                             validators=[validate_icon_image_size])
 
-    def save(self, *args, **kwargs):
-        self.name = self.name.lower()
+    # delete previous icon if a new one is uploaded
+    def save(self, *args, **kwargs) -> None:
+        if self.id:
+            existing = get_object_or_404(Channel, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+
         super(Channel, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Channel")
+    def on_delete_channel_delete_files(self, sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon" or field.name == "banner":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
