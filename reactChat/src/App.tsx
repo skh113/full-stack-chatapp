b98a784926f9ff9813cf76a3a4cbf83866ccdee9
Keyvan Hosseini <skhprogramming@gmail.com>
2023-07-26 18:21:36 +0330

    Added react router

--- reactChat/src/App.tsx
+++ reactChat/src/App.tsx
@@ -1,8 +1,20 @@
-function App() {
-  return (
-    <>
+import Home from "./pages/Home.tsx";
 
-    </>
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import React from "react";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    // loader: rootLoader,
+  },
+])
+
+
+const App: React.FC = () => {
+  return (
+    <RouterProvider router={router}/>
   )
 }
 
